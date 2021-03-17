c4ec92563935f43bedb83ba82b4a4aba031b9f0d
Vicky Jones <vlj8008@gmail.com>
2021-03-17 09:13:30 -0400

    Step 101 JS Dictionary Submission
    Minimum project requirements:
    
    Write a dictionary with a function that outputs a key-value pair into an HTML element
    
    Write a delete statement that removes the key before its value is displayed
    
    Add comments throughout your JavaScript explaining your code

--- Project4_dictionaries/JS/main.js
+++ Project4_dictionaries/JS/main.js
@@ -1,10 +1,14 @@
-function my_dictionaray(){ //assigning my function a name. 
+function my_dictionary(){ //assigning my function a name. 
     var Horse = {//assigning a variable "horse"
-    Species: "Horse",
+    Species: "Horse",  //assigning some key value items
     Color: "Brown",
     Breed: "Thoroughbred",
-    Age: 5,
+    Age: 5, // no need for quotation marks as data type is numerical. 
     Name: "Rosie"
     };
-    document.getElementById("Dictionary").innerHTML = Horse.Breed;
+    document.getElementById("dictionary").innerHTML = Horse.Breed; // inserts the horse breed (thoroughbred) in to the HTML element that has 
+    //the ID "dictionary"
+    delete Horse.Color;
+    document.getElementById("col").innerHTML = Horse.Color;
+
 }
